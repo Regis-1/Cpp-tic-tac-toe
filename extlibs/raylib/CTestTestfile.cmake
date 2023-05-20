f6263e45ab9c923a54c413779ddd556d81506f9d
Regis-1 <jdudzinski101@gmail.com>
2023-05-20 13:12:13 +0200

    initial commit w/ raylib

---
+++ extlibs/raylib/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/hades/development/libraries_main/raylib
+# Build directory: /home/hades/development/libraries_main/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
+subdirs("examples")
---
+++ extlibs/raylib/raylib/CMakeFiles/raylib.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hades/development/libraries_main/raylib/src/raudio.c" "raylib/CMakeFiles/raylib.dir/raudio.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/raudio.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/rcore.c" "raylib/CMakeFiles/raylib.dir/rcore.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/rcore.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/rmodels.c" "raylib/CMakeFiles/raylib.dir/rmodels.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/rmodels.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/rshapes.c" "raylib/CMakeFiles/raylib.dir/rshapes.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/rshapes.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/rtext.c" "raylib/CMakeFiles/raylib.dir/rtext.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/rtext.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/rtextures.c" "raylib/CMakeFiles/raylib.dir/rtextures.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/rtextures.c.o.d"
+  "/home/hades/development/libraries_main/raylib/src/utils.c" "raylib/CMakeFiles/raylib.dir/utils.c.o" "gcc" "raylib/CMakeFiles/raylib.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
